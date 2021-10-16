beabc49b849cbdd6bd1840d4f5b201dc63f46698
imdarsh <shindesahil63@gmail.com>
2021-10-16 18:54:29 +0530

    Sidebar Update and Create functionality

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
 # rooms = [
@@ -10,8 +10,12 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(topic__name__icontains=q)
+    topics = Topic.objects.all()
+
+    context = {'rooms':rooms,'topics':topics}
     return render(request,'core/home.html',context)
 
 def room(request, pk):
@@ -21,6 +25,7 @@ def room(request, pk):
 
 def createRoom(request):
     form = RoomForm()
+    
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
@@ -29,3 +34,24 @@ def createRoom(request):
 
     context = {'form': form}
     return render(request, 'core/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    context = {'form': form}
+    return render(request, 'core/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'core/delete.html', {'obj': room})
